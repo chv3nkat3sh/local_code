5d86d027bcbb6c23f3e42d73d43974e68f02edc3
chv3nkat3h <chv3nkat3sh@gmail.com>
2026-07-05 16:33:51 +0530

    abc

--- divison.py
+++ divison.py
@@ -1,9 +1,5 @@
 a=10
 b=2
 c=a/b
-<<<<<<< HEAD
 print('Divsion by two number c:',c) # divison by two number
-print(c)
-=======
 print('Divsion by two number c:',c) # divison by two number
->>>>>>> 2cf2636f19ff4da2fdf6b4eb64626689eaf59d3e
