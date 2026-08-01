5bf610c0b9feb1c60f7be6d2428535f395571b02
chv3nkat3h <chv3nkat3sh@gmail.com>
2026-08-02 03:12:23 +0530

    Commiting changes

--- divison.py
+++ divison.py
@@ -2,4 +2,4 @@ a=10
 b=2
 c=a/b
 print('Divsion by two number c:',c) # divison by two number
-print('Divsion by two number c:',c) # divison by two numberq
+print('Divsion by two number c:',c) # divison by two number
